e31b6ed5c8990d887e10eb04b83c28f4e383a8df
asif <asifalishaik415@gmail.com>
2026-02-23 23:05:18 -0800

    terraform

---
+++ count/output.tf
@@ -0,0 +1,17 @@
+/* output "roboshop_instances"{
+    value = aws_instance.example
+    description = "description"
+}  */
+  
+
+  output "fruits-list"{
+
+    value = var.fruits-list
+  }  
+  
+
+  output "fruits" {
+
+    value = var.fruits-set
+    
+  }
--- count/variables.tf
+++ count/variables.tf
@@ -1,5 +1,32 @@
-variable "instances" {
+ variable "instances" {
+   type = list 
+   default = ["mongodb","reddis","mysql","reddis","rabbitmq","catalogue","user","cart","shippping","payment","frontend"]
+ }
 
-  type = list 
-  default = ["mongodb","reddis","mysql","reddis","rabbitmq","catalogue","user","cart","shippping","payment","frontend"]
-}
+
+ variable "zone_id" {
+
+    default = "Z07332502BP26TE2VLLYI"
+   
+ }
+
+ variable "domain_name" {
+
+    default = "shaiks.online"
+
+ }
+
+
+ variable "fruits-list" {
+
+    type = list(string)
+    default = ["mango","apple","berries","apple"] 
+   
+ }
+
+
+ variable "fruits-set" {
+
+    type = set(string)
+    default = ["mango","apple","berries","apple"] 
+ }
