3a54ecc3defb48330cec5a50a42dccbdba530327
asif <asifalishaik415@gmail.com>
2026-02-28 23:41:37 -0800

    terraform

--- locals/variables.tf
+++ locals/variables.tf
@@ -8,7 +8,22 @@ variable "environment" {
   default     = "dev"
 }
 
-variable "instance_name" {
-    type = string
-    default = "${var.name}-${var.environment}" # locals-dev
-} 
+# variable "instance_name" {
+#     type = string
+#     default = "${var.name}-${var.environment}" # locals-dev
+# } 
+
+
+
+variable "ec2_tags" {
+    default = {
+        Name = "locals-demo"
+        Environment = "prod"
+    }
+}
+
+variable "sg_tags" {
+    default = {
+        Name = "locals-demo"
+    }
+}
